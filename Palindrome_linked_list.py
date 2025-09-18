edea5e3d4d4abcfd43b2b4343d339d3e194fafb3
divya-123-TC <d76469224@gmail.com>
2025-09-18 20:35:28 +0530

    Add files via upload

---
+++ Palindrome_linked_list.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        slow=head
+        fast=head
+        while fast and fast.next:
+            fast=fast.next.next
+            slow=slow.next
+        node=None
+        while slow:
+            temp=slow.next
+            slow.next=node
+            node=slow
+            slow=temp
+            first,second=head,node
+        while second:
+            if first.val!=second.val:
+                return False
+            first=first.next
+            second=second.next
+        return True
