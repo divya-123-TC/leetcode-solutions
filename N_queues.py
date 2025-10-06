d4477443883506746f046066993ea335bb68bf78
divya-123-TC <d76469224@gmail.com>
2025-10-06 20:22:18 +0530

    Add files via upload

---
+++ N_queues.py
@@ -0,0 +1,37 @@
+class Solution:
+    def solveNQueens(self, n: int):
+        res = []
+        board = [["."] * n for _ in range(n)]
+        
+        # sets to track used columns and diagonals
+        cols = set()
+        pos_diag = set()  # (r + c)
+        neg_diag = set()  # (r - c)
+        
+        def backtrack(r):
+            if r == n:
+                # convert board to list of strings
+                res.append(["".join(row) for row in board])
+                return
+            
+            for c in range(n):
+                if c in cols or (r + c) in pos_diag or (r - c) in neg_diag:
+                    continue
+                
+                # place queen
+                board[r][c] = "Q"
+                cols.add(c)
+                pos_diag.add(r + c)
+                neg_diag.add(r - c)
+                
+                # go to next row
+                backtrack(r + 1)
+                
+                # backtrack
+                board[r][c] = "."
+                cols.remove(c)
+                pos_diag.remove(r + c)
+                neg_diag.remove(r - c)
+        
+        backtrack(0)
+        return res
