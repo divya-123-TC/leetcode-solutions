5e1c3c808a7771463935fe910dd09a9c3c93d9d8
divya-123-TC <d76469224@gmail.com>
2025-10-24 16:16:47 +0530

    Add files via upload

---
+++ Path_sum2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        res=[]
+        def dfs(node,path,curr_sum):
+            if not node:
+                return
+            path.append(node.val)
+            curr_sum+=node.val
+            if not node.left and not node.right and curr_sum==targetSum:
+                res.append(list(path))
+            dfs(node.left,path,curr_sum)
+            dfs(node.right,path,curr_sum)
+            path.pop()
+        dfs(root,[],0)
+        return res
