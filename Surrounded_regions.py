0fa9d461b45b21d333b8288f805d6e294d5f8b95
divya-123-TC <d76469224@gmail.com>
2025-10-30 20:00:33 +0530

    Add files via upload

---
+++ Surrounded_regions.py
@@ -0,0 +1,21 @@
+ROWS,COLS=len(board),len(board[0])
+        def capture(r,c):
+            if (r<0 or c<0 or r==ROWS or c==COLS or board[r][c]!="0"):
+                return
+            board[r][c]="T"
+            capture(r+1,c)
+            capture(r-1,c)
+            capture(r,c+1)
+            capture(r,c-1)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (board[r][c]=="0" and (r in[0,ROWS-1] or c in [0,COLS-1])):
+                    capture(r,c)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c]=="0":
+                    board[r][c]="X"
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c]=="T":
+                    board[r][c]="0"
